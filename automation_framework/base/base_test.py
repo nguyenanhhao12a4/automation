962ede56977126b34fe3ca9ded278b5059e2e564
nguyenanhhao12a4 <nguyenanhhao12a4@gmail.com>
2025-09-13 20:50:14 +0700

    phan chia thu muc va code lai login

--- automation_framework/base/base_test.py
+++ automation_framework/base/base_test.py
@@ -1,14 +1,21 @@
 import pytest
 from selenium import webdriver
 from time import sleep
-
+from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
 class BaseTest:
     @pytest.fixture(scope="class", autouse=True)
     def setup(self, request):
         driver = webdriver.Chrome()
         driver.maximize_window()
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        sleep(2)
+        
+        #xac nhan trang login hien thi
+        login_page = LoginPage(driver)
+        assert login_page.is_login_page_displayed(), "Login page is not displayed"
+        print("Login page is displayed")
+        
         request.cls.driver = driver
+
         yield 
         driver.quit()
--- automation_framework/tests/test_login.py
+++ automation_framework/tests/test_login.py
@@ -1,68 +1,56 @@
+import time
 import pytest
 from base.base_test import BaseTest
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
+
 @pytest.mark.usefixtures("setup")
 class TestOrange(BaseTest):
+    
     def test_login(self):
-        driver = self.driver
-
-        # Chờ đến khi trường username xuất hiện
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//input[@name='username']"))
-        )
-
-        # Nhập tên đăng nhập
-        username_field = driver.find_element(By.XPATH, "//input[@name='username']")
-        username_field.send_keys("Admin")
-
-        # Nhập mật khẩu
-        password_field = driver.find_element(By.XPATH, "//input[@name='password']")
-        password_field.send_keys("admin123")
-
-        # Nhấn nút đăng nhập
-        login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-        login_button.click()
-
-        # Chờ đến khi trang dashboard tải xong
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']"))
-        )
-        time.sleep(4)
-        print("Login successful")
-    def test_add_vacancy(self):
-        driver = self.driver
+        login_page = LoginPage(self.driver)
+        login_page.login("Admin", "admin123")
         
-        # Nhấn vào menu "Recruitment"
-        recruitment_menu = driver.find_element(By.XPATH, "//span[text()='Recruitment']")
-        recruitment_menu.click()
-
-        # Chờ đến khi trang Recruitment tải xong
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']"))
-        )
-
-        # Nhấn vào nút "vacancies"
-        vacancies_button = driver.find_element(By.XPATH, "//a[@class='oxd-topbar-body-nav-tab-item' and text()='Vacancies']")
-        vacancies_button.click()
-
-        # Chờ đến khi trang vacancies tải xong
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//h5[@class='oxd-text oxd-text--h5 oxd-table-filter-title']"))
-        )
-        time.sleep(4)
-        print("Add vacancy successful")
+        #xac nhan dang nhap thanh cong va hien thi dashboard
+        dashboard_page = DashboardPage(self.driver)
+        assert dashboard_page.is_dashboard_displayed(), "Dashboard is not displayed after login"
+        print("Dashboard is displayed !!!")
+        print("Login successful !!!")
+    
+    # def test_add_vacancy(self):
+    #     driver = self.driver
         
-        # Nhấn vào nút "Add"
-        add_button = driver.find_element(By.XPATH, "//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")
-        add_button.click()
+    #     # Nhấn vào menu "Recruitment"
+    #     recruitment_menu = driver.find_element(By.XPATH, "//span[text()='Recruitment']")
+    #     recruitment_menu.click()
 
-        # Chờ đến khi trang thêm vị trí tuyển dụng tải xong
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//h6[@class='oxd-text oxd-text--h6 orangehrm-main-title']"))
-        )
-        time.sleep(4)
-        print("Add vacancy successful")
+    #     # Chờ đến khi trang Recruitment tải xong
+    #     WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.XPATH, "//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']"))
+    #     )
+
+    #     # Nhấn vào nút "vacancies"
+    #     vacancies_button = driver.find_element(By.XPATH, "//a[@class='oxd-topbar-body-nav-tab-item' and text()='Vacancies']")
+    #     vacancies_button.click()
+
+    #     # Chờ đến khi trang vacancies tải xong
+    #     WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.XPATH, "//h5[@class='oxd-text oxd-text--h5 oxd-table-filter-title']"))
+    #     )
+    #     time.sleep(4)
+    #     print("Add vacancy successful")
+        
+    #     # Nhấn vào nút "Add"
+    #     add_button = driver.find_element(By.XPATH, "//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")
+    #     add_button.click()
+
+    #     # Chờ đến khi trang thêm vị trí tuyển dụng tải xong
+    #     WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.XPATH, "//h6[@class='oxd-text oxd-text--h6 orangehrm-main-title']"))
+    #     )
+    #     time.sleep(4)
+    #     print("Add vacancy successful")
         
