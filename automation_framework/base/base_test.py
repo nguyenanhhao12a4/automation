ca2a6b9a7a00d44ccbf3856c59f3508207ef9d58
nguyenanhhao12a4 <nguyenanhhao12a4@gmail.com>
2025-09-20 19:36:35 +0700

    them ci cd

--- automation_framework/base/base_test.py
+++ automation_framework/base/base_test.py
@@ -6,15 +6,19 @@ from pages.dashboard_page import DashboardPage
 class BaseTest:
     @pytest.fixture(scope="class", autouse=True)
     def setup(self, request):
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=options)
         driver.maximize_window()
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        
+
         #xac nhan trang login hien thi
         login_page = LoginPage(driver)
         assert login_page.is_login_page_displayed(), "Login page is not displayed"
         print("Login page is displayed")
-        
+
         request.cls.driver = driver
 
         yield 
